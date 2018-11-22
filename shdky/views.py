cd6542c3584baf7b6ffc16e75b4ae49d09f2bdf3
Jiang Feng demoweb <biljiang@outlook.com>
2018-11-22 20:36:44 +0800

    ajax served with tcp connect

--- shdky/views.py
+++ shdky/views.py
@@ -108,6 +108,21 @@ class PGMonitor(TemplateView):
     from bokeh.plotting import Figure, output_file, show
     from bokeh.embed import components
     from bokeh.models.sources import AjaxDataSource
+    from bokeh.models import DatetimeTickFormatter
+
+    ''' 
+    ### add adatper func to adjust timezone for browsers
+    adapter = CustomJS(code="""
+        var result = {time: [], AMBIENT_TEMPERATURE: [], AMBIENT_TEMPERATURE_simu: [], AMBIENT_TEMPERATURE_dif: []}
+        const rps = cb_data.response
+        result.time = rps.time
+        result.AMBIENT_TEMPERATURE = rps.AMBIENT_TEMPERATURE
+        result.AMBIENT_TEMPERATURE_simu = rps.AMBIENT_TEMPERATURE_simu
+        result.AMBIENT_TEMPERATURE_dif = rps.AMBIENT_TEMPERATURE_dif
+        return result
+    """)
+    '''
+     
     #source = AjaxDataSource(data_url='https://182.150.63.68:10443/static/test.json',method='GET') # failed
     #source = AjaxDataSource(data_url='http://localhost:9090/static/test.json',method='GET') # failed, it seems unsuccessful when serving files directly
     source = AjaxDataSource(data_url='https://182.150.63.68:10443/shdky/pg_ajax',method="GET",polling_interval=5000)
@@ -116,10 +131,14 @@ class PGMonitor(TemplateView):
     #    'y' : [9, 3, 2]
     #    }
     #source = ColumnDataSource(data=data)    
-    p = Figure(plot_width=800, plot_height=300,x_axis_type="datetime")
+    p = Figure(plot_width=800, plot_height=300,x_axis_type="datetime",title='AMBIENT_TEMPERATURE')
     p.line('time', 'AMBIENT_TEMPERATURE', source=source, line_width=2, line_alpha=0.3, line_color="red")
     p.line('time', 'AMBIENT_TEMPERATURE_simu', source=source, line_width=2, line_alpha=0.3, line_color="blue")
     p.line('time', 'AMBIENT_TEMPERATURE_dif', source=source, line_width=2, line_alpha=0.3, line_color="yellow")
+    p.title.align  ="center"
+    p.yaxis.axis_label = 'AMBIENT_TEMPERATURE'
+    p.xaxis.axis_label = 'TIME'
+    p.xaxis[0].formatter = DatetimeTickFormatter(minutes=["%X"])
     layout = column(p)
     script,div = components(layout)
 
@@ -157,7 +176,7 @@ def v_simu_table(request):
     df.to_html(tb)
     return HttpResponse(tb.getvalue())
 
-def pg_ajax(request):
+def pg_ajax_file(request):
 #    if request.method == 'POST':
     import json
 #    J={ 'x' : [np.random.randint(5), np.random.randint(5), np.random.randint(5)],
@@ -186,11 +205,69 @@ def pg_ajax(request):
     df_out= df.iloc[:,0:1].join(T_res.iloc[:,0:1],rsuffix="_simu").join(diff.iloc[:,0:1],rsuffix="_dif")
 
     df_out['time']=pd.to_datetime(df_out.index)
-    df_out['time']=df_out['time'].apply(lambda x: x.value/1000000)
+    df_out['time']=df_out['time'].apply(lambda x: x.value/1000)
 
 
     J = {S: list(df_out[S].values) for S in df_out}
     return HttpResponse(json.dumps(J))
 #    return HttpResponse('Get method used or somthing wrong')
  
+def pg_ajax(request):
+    import json
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 建立连接:
+    s.connect(('127.0.0.1', 9999))
+    s.send(b'ready to receive data')
+    # 接收数据:
+    s.recv(1024) # receive welcome message
+    buffer = []
+    while True:
+        # 每次最多接收1k字节:
+        d = s.recv(1024)
+        if d:
+            buffer.append(d)
+        else:
+            break
+    data = b''.join(buffer)
+    s.close()
+    
+#### read dynamic dataframe as T
+    df = pd.read_json(data.decode('utf-8'))
+    #df.time=df.time.apply(int)
+    df = df.set_index('time')
+
+#### compute simu_values with status_matrix,T_ min_max and G_inv
+    T_MIN = T_min_max.T_min; T_MAX = T_min_max.T_max
+    df_normal = (df-T_MIN)/(T_MAX-T_MIN)
+    T_A = np.array(df_normal)
+
+
+
+    Y_list=[]
+    for j in range(len(df)):
+        w=np.array([[np.linalg.norm(T_A[j,:]-status_matrix[:,i])] for i in range(status_matrix.shape[1])])
+        Y_list.append(np.dot(status_matrix,np.dot(G_inv,w))[:,0])
+    Target_array = np.array(Y_list)
+    Target = pd.DataFrame(Target_array,index=df.index,columns=df.columns)
+    T_res = (Target*(T_MAX-T_MIN)+T_MIN)
+    diff = df - T_res
+    df_out= df.iloc[:,0:1].join(T_res.iloc[:,0:1],rsuffix="_simu").join(diff.iloc[:,0:1],rsuffix="_dif")
+
+    df_out = df_out.reset_index()
+    df_out['time']=df_out['time'].apply(lambda x: x/1.0)
+
+
+    J = {S: list(df_out[S].values) for S in df_out}
+    return HttpResponse(json.dumps(J))
+    
+    ### use following return will draw the monitor line only
+    #return HttpResponse(data.decode('utf-8'))
+
+
+
+
+
+
+
 
